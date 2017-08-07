0e452878b62c4cc843ab00a7ac97f2c22eb3ba45
Brandon Mills <sllimnodnarb@gmail.com>
2017-08-07 14:02:44 -0500

    adding changes to stack and queue

--- 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
+++ 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
@@ -5,14 +5,24 @@ class MyQueue
   def initialize
     @queue = Array.new
     @head = @queue[0]
+    @tail = @queue[queue.length - 1]
   end
 
   def enqueue(element)
+    @queue.unshift(element)
+    @head = @queue[0]
+    @tail = @queue[queue.length - 1]
   end
 
   def dequeue
+    @queue.shift
+    @head = @queue[0]
+    @tail = @queue[queue.length - 1]
   end
 
   def empty?
+    if @queue.length != 0
+      return false
+    end
   end
 end
--- 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
+++ 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
@@ -4,14 +4,25 @@ class MyStack
   def initialize
     @stack = Array.new
     self.top = nil
+    @top = stack[0]
+    @bottom = stack.length - 1
   end
 
   def push(item)
+    @stack << item
+    @top = stack[0]
+    @bottom = stack.length - 1
   end
 
   def pop
+    @stack.pop
+    @top = stack[0]
+    @bottom = stack.length - 1
   end
 
   def empty?
+    if stack.count != 0
+      return false
+    end
   end
 end
