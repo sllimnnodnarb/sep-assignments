53ef5255f52f9f929f5029a11460dc43c94ae2f3
Brandon Mills <sllimnodnarb@gmail.com>
2017-08-07 11:32:09 -0500

    adding intro to data structures files

--- 01-data-structures/01-introduction-to-data-structures/screen/screen.rb
+++ 01-data-structures/01-introduction-to-data-structures/screen/screen.rb
@@ -6,18 +6,36 @@ class Screen
   attr_accessor :matrix
 
   def initialize(width, height)
+    @grid = Arr.new(width) { Arr.new(height) }
   end
 
   # Insert a Pixel at x, y
   def insert(pixel, x, y)
+    if inbounds(x, y)
+      @grid[x][y] = pixel
+    else
+      puts "x/y values out out of bounds"
+    end
   end
 
   def at(x, y)
+    if inbounds(x, y)
+      return @grid[x][y]
+    else
+      puts "x/y values out out of bounds"
+    end
   end
 
   private
 
   def inbounds(x, y)
+    if x < 0 || x > @width-1
+     puts "x value is out of bounds"
+    end
+
+    if y < 0 || y > @height-1
+      puts "y value is out of bounds"
+    end
   end
 
 end
