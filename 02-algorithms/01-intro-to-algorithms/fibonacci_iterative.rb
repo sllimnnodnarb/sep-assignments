563f7b7e1eacf0d3a48bc0996bb19ca694fe5dce
Brandon Mills <sllimnodnarb@gmail.com>
2017-08-17 12:59:01 -0500

    adding files for intro to algos

---
+++ 02-algorithms/01-intro-to-algorithms/fibonacci_iterative.rb
@@ -0,0 +1,24 @@
+def fib(n)
+  if n < 1 || n % 1 != 0
+    'Invalid Number. Input a sequence of at least 1.'
+  else
+    fib_0 = 0
+    fib_1 = 1
+    temp = 0
+    n.times do
+      temp = fib_0 + fib_1
+      fib_0 = fib_1
+      fin_1 = temp
+    end
+   puts fib(0)
+   puts fib(1)
+   puts fib(2)
+   puts fib(3)
+   puts fib(4)
+   puts fib(5)
+   puts fib(6)
+   puts fib(7)
+   puts fib(8)
+   puts fib(9)
+  end
+end
