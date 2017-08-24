e64593597a060def68b99800c24a3f2bf06e83ab
Brandon Mills <sllimnodnarb@gmail.com>
2017-08-24 13:17:52 -0500

    complexity answers

--- 02-algorithms/02-search-algorithms/binary_search_iterative.rb
+++ 02-algorithms/02-search-algorithms/binary_search_iterative.rb
@@ -16,3 +16,21 @@ class Search
     value == arr[start]
   end
 end
+
+#Bloc implementation
+#def binary_search_iterative(collection, val)
+  # #1
+#  low, high = 0, collection.length - 1
+#  # #2
+#  while low <= high
+#    mid = ((low + high) / 2).ceil
+#    if val < collection[mid]
+#      low = mid + 1
+#    elsif val > collection[mid]
+#      high = mid - 1
+#    else
+#      return mid
+#    end
+#  end
+#  nil
+#end
